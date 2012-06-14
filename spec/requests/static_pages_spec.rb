148e4058704abe029804be15c8d3919dd0d7e3e6
Justin T Kim <tjk.abdn@gmail.com>
2012-06-14 15:42:54 +0100

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,13 +5,40 @@ describe "StaticPages" do
 	subject { page }
 	
 	describe "Home Page" do
-
 		before{ visit '/static_pages/home' }
 
-		
-		#visit '/static_pages/home'
 		it {should have_selector('title', text: "HOME")}
 		it {should have_selector('h1', text: "HOME")}
-	
 	end
+
+	describe "Planning" do
+		before{ visit '/static_pages/planning' }
+
+		it { should have_selector('title', text: "PLANNING") }
+		it { should have_selector('h1', text: "PLANNING") }
+	end
+
+
+	describe "Scheduling" do
+		before{ visit '/static_pages/scheduling' }
+
+		it { should have_selector('title', text: "SCHEDULING") }
+		it { should have_selector('h1', text: "SCHEDULING") }
+	end
+
+	describe "People" do
+		before{ visit '/static_pages/people'}
+
+		it{ should have_selector('title', text: "PEOPLE") }
+		it{ should have_selector('h1', text: "PEOPLE") }
+	end
+
+	describe "Library" do
+		before{ visit '/static_pages/library' }
+
+		it{ should have_selector('title', text: "LIBRARY") }
+		it{ should have_selector('h1', text: "LIBRARY") }
+	end
+
+
 end
