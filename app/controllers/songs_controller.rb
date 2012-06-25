104f8a86a1b1c9ab17874ac11ccf529e5ca51b62
Justin T Kim <tjk.abdn@gmail.com>
2012-06-25 14:26:41 +0100

    add index to songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,10 @@ class SongsController < ApplicationController
   	@song = Song.new
   end
 
+  def index
+    @songs = Song.all
+  end
+
   def create
   	@song = Song.new(params[:song])
   	if @song.save
