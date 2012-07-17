05d4fbcd5a258d763dc0c25edabc531c2ba026e1
Justin T Kim <tjk.abdn@gmail.com>
2012-07-17 13:34:43 +0100

    remove a few redundant files

--- app/models/categorizations.rb
+++
@@ -1,8 +0,0 @@
-class Categorizations < ActiveRecord::Base
-  attr_accessible :category_id, :song_id
-
-  validates :category_id, presence: true
-  validates :song_id, presence: true
-
-  validates_uniqueness_of :category_id, :scope => :song_id
-end
